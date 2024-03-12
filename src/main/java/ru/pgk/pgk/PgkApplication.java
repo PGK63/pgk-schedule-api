dc2987ca7f03317c224135ce65c53d591e2b12c2
Данила Беляков <danilabelakov@MacBook-Air-von-Danila.local>
2024-03-12 11:11:40 +0400

    time zone

--- src/main/java/ru/pgk/pgk/PgkApplication.java
+++ src/main/java/ru/pgk/pgk/PgkApplication.java
@@ -1,16 +1,24 @@
 package ru.pgk.pgk;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableCaching
 @EnableScheduling
 public class PgkApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Samara"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PgkApplication.class, args);
