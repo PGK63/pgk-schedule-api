3c2e54d8ea3a95d49c0f4ed704ba7a7362038e19
Данила Беляков <danilabelakov@MacBook-Air-von-Danila.local>
2024-08-22 01:30:16 +0400

    remove api token

--- src/main/java/ru/pgk/pgk/features/apiToken/mappers/ApiTokenMapper.java
+++
@@ -1,9 +0,0 @@
-package ru.pgk.pgk.features.apiToken.mappers;
-
-import org.mapstruct.Mapper;
-import ru.pgk.pgk.common.mapper.Mappable;
-import ru.pgk.pgk.features.apiToken.dto.ApiTokenDto;
-import ru.pgk.pgk.features.apiToken.entities.ApiTokenEntity;
-
-@Mapper(componentModel = "spring")
-public interface ApiTokenMapper extends Mappable<ApiTokenEntity, ApiTokenDto> {}
