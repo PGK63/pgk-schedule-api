490010fb86e933f512398cd1b512ff70a31b077b
Данила Беляков <danilabelakov@MacBook-Air-von-Danila.local>
2024-08-21 22:28:15 +0400

    eureka-server

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM openjdk:17.0.2-jdk-slim
 COPY build/libs/pgk-1.0.0-SNAPSHOT.jar application.jar
+COPY build/libs/.env .env
 ENTRYPOINT ["java", "-jar", "application.jar"]
--- src/main/java/ru/pgk/pgk/PgkApplication.java
+++ src/main/java/ru/pgk/pgk/PgkApplication.java
@@ -4,24 +4,26 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.TimeZone;
 
-@SpringBootApplication
-@EnableTransactionManagement
 @EnableCaching
 @EnableScheduling
+@EnableDiscoveryClient
+@EnableTransactionManagement
+@SpringBootApplication
 public class PgkApplication {
 
-	@PostConstruct
-	void started() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Samara"));
-	}
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Samara"));
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(PgkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PgkApplication.class, args);
+    }
 
 }	
